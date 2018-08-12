c43375a2aa328f4d1f4b673afd7b366d27ccae97
Arimos <amilinkovic.ns@gmail.com>
2018-08-12 21:42:51 +0200

    zadatak 6

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -1,8 +1,12 @@
 <template>
   <div >
     <h1>Products</h1>
+    <div class="search-wrapper">
+      <label>Search products:</label>
+      <input type="text" v-model="searchProducts" placeholder="Search products"/>
+    </div>
     <table class="table">
-        <tr v-for="product in products" :key="product.id">
+        <tr v-for="product in products" :key="product.id" v-if="product.name.toLowerCase().includes(searchProducts.toLowerCase())">
           <td>{{product.name}}</td>
         </tr>
     </table>
@@ -14,7 +18,8 @@ import { productService } from '../services/ProductService.js'
 export default {
   data(){
     return {
-    products: productService.listProducts()
+    products: productService.listProducts(),
+    searchProducts: ''
     }
   }
   
